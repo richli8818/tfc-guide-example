343ef659a3002da47cf1b36261c453e07a82c0b2
Richard Li <richli8818@gmail.com>
2021-04-18 12:45:45 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.27"
+    }
+  }
+}
+
+
 provider "aws" {
+  profile = "default"
   region = var.aws_region
 }
 
